95d99227f6c6731342ab2f326f64235daedb814c
ArgusCode <89656571+ArgusCode@users.noreply.github.com>
2022-02-28 10:18:57 -0800

    Update ps1a.py

--- ps1a.py
+++ ps1a.py
@@ -1,5 +1,9 @@
 # Ps1a.py
 
+annual_return = 0.04
+current_savings = 0.0
+number_of_months = 0
+
 annual_salary = float(input('Enter your annual salary: '))
 portion_saved = float(input('Enter the percent of your salary to save, as a decimal: '))
 monthly_savings = (annual_salary / 12) * portion_saved
@@ -7,10 +11,6 @@ monthly_savings = (annual_salary / 12) * portion_saved
 total_cost = float(input('Enter the cost of your dream home: '))
 portion_down_payment = 0.25
 down_payment = total_cost * portion_down_payment
-
-annual_return = 0.04
-current_savings = 0.0
-number_of_months = 0
 
 while current_savings < down_payment:
     current_savings += monthly_savings + ((current_savings * annual_return) / 12)
