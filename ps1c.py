4360487689ccbfe082f629d9a4bf543b43785631
ArgusCode <89656571+ArgusCode@users.noreply.github.com>
2022-02-28 08:26:59 -0800

    Update ps1c.py

--- ps1c.py
+++ ps1c.py
@@ -1,1 +1,29 @@
+# ps1c.py
 
+def calculate_savings_rate(annual_salary):
+
+annual_salary = float(input("Enter your starting annual salary: "))
+portion_saved = ????
+
+total_cost = 1000000.0
+semi_annual_raise = .07
+portion_down_payment = 0.25 * total_cost
+current_savings = 0
+r = 0.04
+
+months = 0
+
+while(months < 36):
+    
+    monthly_salary = annual_salary / 12
+    
+    current_savings = current_savings + (current_savings * r / 12) + (
+            calculate_savings_rate(annual_salary) * monthly_salary)
+    
+    months += 1
+    
+    if (months % 6) == 0:
+        annual_salary = annual_salary + (annual_salary * semi_annual_raise)
+    
+
+print("Best savings rate: ", portion_saved)
